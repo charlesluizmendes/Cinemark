842614714db3e1da0e6aa49bd8ba2602b6d45043
Charles Mendes <charlesluizmendes@gmail.com>
2022-06-21 10:29:21 -0300

    Finalizando Camada de Domínio

--- Cinemark.Domain/Entities/Filme.cs
+++ Cinemark.Domain/Entities/Filme.cs
@@ -2,5 +2,10 @@
 {
     public class Filme
     {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Categoria { get; set; }
+        public int FaixaEtaria { get; set; }
+        public DateTime DataLancamento { get; set; }
     }
 }
--- Cinemark.Domain/Validators/FilmeValidator.cs
+++ Cinemark.Domain/Validators/FilmeValidator.cs
@@ -7,7 +7,21 @@ namespace Cinemark.Domain.Validators
     {
         public FilmeValidator()
         {
-            
+            RuleFor(x => x.Nome)
+                .NotEmpty().NotNull()
+                .WithMessage("O Nome não pode ser nulo");
+
+            RuleFor(x => x.Categoria)
+                .NotEmpty().NotNull()
+                .WithMessage("A Categoria não pode ser nula");
+
+            RuleFor(x => x.FaixaEtaria)
+               .NotEmpty().NotNull()
+               .WithMessage("A Faixa Etária não pode ser nula");
+
+            RuleFor(x => x.DataLancamento)
+               .NotNull().NotNull()
+               .WithMessage("A Data de Lançamento não pode ser nula");
         }
     }
 }
