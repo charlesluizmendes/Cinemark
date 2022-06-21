1bc18da38062f06a1c4279941a82015b60f7aea8
Charles Mendes <charlesluizmendes@gmail.com>
2022-06-20 23:56:21 -0300

    Criando Interfaces e Services

---
+++ Cinemark.Domain/Interfaces/Services/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿namespace Cinemark.Domain.Interfaces.Services
+{
+    public interface IBaseService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(object id);
+        Task<T> InsertAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        void Dispose();
+    }
+}
