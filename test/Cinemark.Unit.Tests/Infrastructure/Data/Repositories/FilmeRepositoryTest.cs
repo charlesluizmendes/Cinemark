61189fda70ebef8345332e1a54ec4e7ab2700a29
Charles Mendes <charlesluizmendes@gmail.com>
2022-06-24 12:16:31 -0300

    Adicionando InMemoryDbContext

--- test/Cinemark.Unit.Tests/Infrastructure/Data/Repositories/FilmeRepositoryTest.cs
+++ test/Cinemark.Unit.Tests/Infrastructure/Data/Repositories/FilmeRepositoryTest.cs
@@ -1,7 +1,12 @@
 ﻿using Cinemark.Domain.Interfaces.Repositories;
 using Cinemark.Domain.Models;
 using Cinemark.Infrastructure.Data.Context;
+using Cinemark.Infrastructure.Data.Context.Option;
+using Cinemark.Infrastructure.Data.Repositories;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 using Moq;
 using Xunit;
 
@@ -12,8 +17,25 @@ namespace Cinemark.Unit.Tests.Infrastructure.Data.Repositories
         [Fact]
         public async void InsertAsync()
         {
-            var mongoContext = new Mock<MongoContext>();
-            var sqlServercontext = new Mock<SqlServerContext>();
+            var mongoConfiguration = new MongoConfiguration()
+            {
+                Connection = "mongodb://tes123 ",
+                DatabaseName = "TestDB"
+            };
+
+            var mongoOptionsMock = new Mock<IOptions<MongoConfiguration>>();
+            mongoOptionsMock.Setup(s => s.Value).Returns(mongoConfiguration);         
+            
+            var mongoContext = new MongoContext(mongoOptionsMock.Object);
+
+            var sqlServerOptions = new DbContextOptionsBuilder<SqlServerContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var sqlServerContext = new SqlServerContext(sqlServerOptions);
+            sqlServerContext.Database.EnsureCreated();
+
+            var filmeRepository = new FilmeRepository(mongoContext, sqlServerContext);
 
             var filme = new Filme()
             {
@@ -23,14 +45,14 @@ namespace Cinemark.Unit.Tests.Infrastructure.Data.Repositories
                 FaixaEtaria = 12,
                 DataLancamento = new DateTime(1971, 10, 3)
             };
-
-            var filmeRepository = new Mock<IFilmeRepository>();
-            filmeRepository.Setup(x => x.InsertAsync(It.IsAny<Filme>()))
-                .ReturnsAsync(filme);
-
-            var result = await filmeRepository.Object.InsertAsync(filme);
+            
+            var result = await filmeRepository.InsertAsync(filme);
 
             result.Id.Should().Be(1);
+            result.Nome.Should().Be("E o Vento Levou");
+            result.Categoria.Should().Be("Drama");
+            result.FaixaEtaria.Should().Be(12);
+            result.DataLancamento.Should().Be(new DateTime(1971, 10, 3));
         }
     }
 }
